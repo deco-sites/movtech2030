36c7679c7967f7409ca8ae4d4283b609478470d6
vinicius-prates <viniprates2211@gmail.com>
2024-09-01 21:21:17 -0300

    new: PartnerTestimonials component

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -9,18 +9,19 @@ import * as $$$$$$1 from "./sections/BlogPosts.tsx";
 import * as $$$$$$2 from "./sections/Faq.tsx";
 import * as $$$$$$3 from "./sections/Footer.tsx";
 import * as $$$$$$4 from "./sections/Header.tsx";
-import * as $$$$$$14 from "./sections/hero-1.tsx";
-import * as $$$$$$15 from "./sections/hero-2.tsx";
+import * as $$$$$$15 from "./sections/hero-1.tsx";
+import * as $$$$$$16 from "./sections/hero-2.tsx";
 import * as $$$$$$5 from "./sections/Hero.tsx";
-import * as $$$$$$16 from "./sections/hero2.tsx";
+import * as $$$$$$17 from "./sections/hero2.tsx";
 import * as $$$$$$6 from "./sections/ImageWithParagraph.tsx";
 import * as $$$$$$7 from "./sections/Logos.tsx";
 import * as $$$$$$8 from "./sections/Manifest.tsx";
 import * as $$$$$$9 from "./sections/Partners.tsx";
-import * as $$$$$$10 from "./sections/Pillars.tsx";
-import * as $$$$$$11 from "./sections/Testimonials.tsx";
-import * as $$$$$$12 from "./sections/Theme/Theme.tsx";
-import * as $$$$$$13 from "./sections/WhoWeAre.tsx";
+import * as $$$$$$10 from "./sections/PartnersTestimonials.tsx";
+import * as $$$$$$11 from "./sections/Pillars.tsx";
+import * as $$$$$$12 from "./sections/Testimonials.tsx";
+import * as $$$$$$13 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$14 from "./sections/WhoWeAre.tsx";
 
 const manifest = {
   "sections": {
@@ -29,18 +30,19 @@ const manifest = {
     "site/sections/Faq.tsx": $$$$$$2,
     "site/sections/Footer.tsx": $$$$$$3,
     "site/sections/Header.tsx": $$$$$$4,
-    "site/sections/hero-1.tsx": $$$$$$14,
-    "site/sections/hero-2.tsx": $$$$$$15,
+    "site/sections/hero-1.tsx": $$$$$$15,
+    "site/sections/hero-2.tsx": $$$$$$16,
     "site/sections/Hero.tsx": $$$$$$5,
-    "site/sections/hero2.tsx": $$$$$$16,
+    "site/sections/hero2.tsx": $$$$$$17,
     "site/sections/ImageWithParagraph.tsx": $$$$$$6,
     "site/sections/Logos.tsx": $$$$$$7,
     "site/sections/Manifest.tsx": $$$$$$8,
     "site/sections/Partners.tsx": $$$$$$9,
-    "site/sections/Pillars.tsx": $$$$$$10,
-    "site/sections/Testimonials.tsx": $$$$$$11,
-    "site/sections/Theme/Theme.tsx": $$$$$$12,
-    "site/sections/WhoWeAre.tsx": $$$$$$13,
+    "site/sections/PartnersTestimonials.tsx": $$$$$$10,
+    "site/sections/Pillars.tsx": $$$$$$11,
+    "site/sections/Testimonials.tsx": $$$$$$12,
+    "site/sections/Theme/Theme.tsx": $$$$$$13,
+    "site/sections/WhoWeAre.tsx": $$$$$$14,
   },
   "apps": {
     "site/apps/decohub.ts": $$$$$$$$$$$0,
---
+++ sections/PartnersTestimonials.tsx
@@ -0,0 +1,120 @@
+import { ImageWidget } from "apps/admin/widgets.ts";
+import Image from "apps/website/components/Image.tsx";
+
+interface Testimonial {
+    name?: string;
+    partnerName?: string;
+    avatar?: ImageWidget;
+    text?: string;
+}
+/**  
+ * @title {{{text}}}
+*/
+interface CTA {
+    id?: string;
+    href: string;
+    text: string;
+    outline?: boolean;
+}
+export interface Props {
+    title?: string;
+    testimonials?: Testimonial[];
+    cta?: CTA[];
+}
+const grafismImage = "https://deco-sites-assets.s3.sa-east-1.amazonaws.com/movtech2030/4d3a808c-c601-49b4-9348-8386c1620424/Linear-Grafism.png"
+const defaultUserIcon =
+    "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/1527/7286de42-e9c5-4fcb-ae8b-b992eea4b78e";
+const defaultText =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada. Nulla facilisi. Pellentesque in ipsum id orci porta dapibus. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus.";
+export default function PartnersTestimonials({
+    title = "Type your title here",
+    testimonials = [{
+        name: "Pessoa X",
+        partnerName: "Coca Cola",
+        avatar: defaultUserIcon,
+        text: defaultText,
+    }, {
+        name: "Pessoa Y",
+        partnerName: "Coca Cola",
+        avatar: defaultUserIcon,
+        text: defaultText,
+    }, {
+        name: "Pessoa Z",
+        partnerName: "Coca Cola",
+        avatar: defaultUserIcon,
+        text: defaultText,
+    }],
+    cta = [{ id: "change-me-1", href: "/", text: "Change me", outline: false }],
+}: Props) {
+    return (
+        <div
+            class={`flex flex-col items-center text-center gap-10 p-10 lg:gap-20 lg:p-[6.25rem] bg-primary bg-no-repeat bg-center`}
+            style={{backgroundImage: `url(${grafismImage})`}}
+        >
+            <h1 class={`text-2xl lg:text-5xl text-info font-bold`}>{title}</h1>
+            <div class={`grid grid-cols-1 lg:grid-cols-2 items-center justify-center gap-10`}>
+
+            <div className="grid grid-cols-1  gap-6">
+                {testimonials.slice(0, 2).map((testimonial, key) => (
+                    <div
+                    key={key}
+                    className={`bg-white p-6 rounded-lg shadow-md flex flex-col`}
+                    >
+                        <p className="text-xl italic text-left mb-4">
+                            "{testimonial.text}"
+                        </p>
+                        <Image
+                            src={testimonial.avatar || ""}
+                            width={40.72}
+                            height={40.72}
+                            class={`rounded-full lg:w-[67.5px] lg:h-[67.5px]`}
+                            />
+                        <div className="mt-auto text-left">
+                            <h4 className="text-md font-bold">
+                                {testimonial.name}
+                            </h4>
+                            <p className="text-md">{testimonial.partnerName}</p>
+                        </div>
+                    </div>
+                ))}
+                
+            </div>
+            <div
+                    className={`bg-white p-6 rounded-lg shadow-md flex flex-col`}
+                    >
+                        <p className="text-xl italic text-left mb-4">
+                            "{testimonials[2].text}"
+                        </p>
+                        <Image
+                            src={testimonials[2].avatar || ""}
+                            width={40.72}
+                            height={40.72}
+                            class={`rounded-full lg:w-[67.5px] lg:h-[67.5px]`}
+                            />
+                        <div className="mt-auto text-left">
+                            <h4 className="text-md font-bold">
+                                {testimonials[2].name}
+                            </h4>
+                            <p className="text-md">{testimonials[2].partnerName}</p>
+                        </div>
+                    </div>
+            </div>
+
+            <div class="flex items-center gap-3">
+              {cta?.map((item) => (
+                <a
+                  key={item?.id}
+                  id={item?.id}
+                  href={item?.href}
+                  target={item?.href.includes("http") ? "_blank" : "_self"}
+                  class={`font-normal btn btn-info text-xl text-primary ${
+                    item.outline && "btn-outline"
+                  }`}
+                >
+                  {item?.text}
+                </a>
+              ))}
+              </div>
+        </div>
+    );
+}
