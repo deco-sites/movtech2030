9a3bb53914de9b8fe852b68ce5acbf3393dd2ae6
vinicius-prates <viniprates2211@gmail.com>
2024-09-02 17:23:19 -0300

    new: own faq

--- sections/Faq.tsx
+++ sections/Faq.tsx
@@ -1,3 +1,5 @@
+import Icon from "site/components/ui/Icon.tsx";
+
 export interface CTA {
   id?: string;
   href: string;
@@ -13,16 +15,12 @@ export interface Question {
 
 export interface Props {
   title?: string;
-  description?: string;
-  cta?: CTA;
+
   questions?: Question[];
 }
 
 export default function BlogPosts({
-  title = "FAQs",
-  description =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.",
-  cta = { id: "change-me", href: "/", text: "Change me", outline: true },
+  title = "Frequent Questions",
   questions = [
     {
       title: "Question #1 text goes here",
@@ -53,53 +51,28 @@ export default function BlogPosts({
 }: Props) {
   return (
     <div class="lg:container md:max-w-6xl lg:mx-auto mx-4 text-sm py-12 lg:py-28">
-      <div class="flex flex-col lg:flex-row gap-10 lg:gap-20 justify-between">
-        <div class="flex-none space-y-6 lg:w-2/5">
-          <p class="text-4xl leading-snug">
-            {title}
-          </p>
-          <p class="text-lg">
-            {description}
-          </p>
-          <a
-            key={cta?.id}
-            id={cta?.id}
-            href={cta?.href}
-            target={cta?.href.includes("http") ? "_blank" : "_self"}
-            class={`font-normal btn btn-primary ${
-              cta.outline && "btn-outline"
-            }`}
-          >
-            {cta?.text}
-          </a>
+      <div class="flex flex-col  gap-10 lg:gap-20 justify-between">
+        <div class="flex- space-y-6  text-center">
+            <h1 class={`text-2xl lg:text-5xl text-accent font-bold`}>{title}</h1>
         </div>
-        <div class="flex-auto border-primary border-t">
+        <div class="flex-auto ">
           {questions?.map((question) => (
-            <details class="border-primary border-b group">
-              <summary class="text-lg cursor-pointer py-6 flex ">
-                <span class="flex-auto">{question.title}</span>
-                <span class="flex-none transition group-open:rotate-180">
-                  <svg
-                    width="32"
-                    height="33"
-                    viewBox="0 0 32 33"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M8.17674 12.5577L8.17676 12.5577L8.5303 12.2041C8.53031 12.2041 8.53031 12.2041 8.53032 12.2041C8.62794 12.1065 8.78621 12.1065 8.88385 12.2041C8.88385 12.2041 8.88385 12.2041 8.88385 12.2041L15.6464 18.9667L16 19.3202L16.3535 18.9667L23.1161 12.2041C23.2138 12.1064 23.372 12.1064 23.4696 12.2041L23.8232 12.5577C23.9208 12.6553 23.9208 12.8135 23.8232 12.9112L16.1767 20.5577C16.0791 20.6553 15.9209 20.6553 15.8232 20.5577L8.17674 12.9112C8.17674 12.9112 8.17674 12.9112 8.17674 12.9112C8.07911 12.8135 8.07911 12.6553 8.17674 12.5577Z"
-                      fill="#18181B"
-                      stroke="#18181B"
-                    />
-                  </svg>
+            <details class="group  ">
+              <summary class="text-lg border-primary items-center  border my-2 rounded-[4px]  text-info bg-primary cursor-pointer py-3 px-5 flex group-open:bg-info group-open:text-primary transition-all  ">
+                <span class="flex-auto font-bold text-sm lg:text-xl">{question.title}</span>
+                <span class="flex text-info transition group-open:hidden">
+                <Icon id="Plus" width={20} height={10}/>
+                </span>
+                <span class="hidden text-primary transition group-open:flex">
+                <Icon id="Minus" width={10} height={5}/>
                 </span>
               </summary>
               <p
-                class="leading-relaxed mb-6 group-open:animate-fadeIn"
+                class="leading-relaxed border-primary  border my-5 rounded-[4px] p-8 bg-primary group-open:animate-fadeIn italic group-open:bg-info text-accent text-sm lg:text-xl trasition-all"
                 dangerouslySetInnerHTML={{ __html: question.answer }}
               >
               </p>
-            </details>
+            </details>  
           ))}
         </div>
       </div>
