083387d4e6d7daf37ab8e487ff8eb1bc9c644b5d
vinicius-prates <viniprates2211@gmail.com>
2024-09-03 13:33:39 -0300

    new: Impact component

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -11,15 +11,16 @@ import * as $$$$$$3 from "./sections/Footer.tsx";
 import * as $$$$$$4 from "./sections/Header.tsx";
 import * as $$$$$$5 from "./sections/Hero.tsx";
 import * as $$$$$$6 from "./sections/ImageWithParagraph.tsx";
-import * as $$$$$$7 from "./sections/InTheMedia.tsx";
-import * as $$$$$$8 from "./sections/Logos.tsx";
-import * as $$$$$$9 from "./sections/Manifest.tsx";
-import * as $$$$$$10 from "./sections/Partners.tsx";
-import * as $$$$$$11 from "./sections/PartnersTestimonials.tsx";
-import * as $$$$$$12 from "./sections/Pillars.tsx";
-import * as $$$$$$13 from "./sections/Testimonials.tsx";
-import * as $$$$$$14 from "./sections/Theme/Theme.tsx";
-import * as $$$$$$15 from "./sections/WhoWeAre.tsx";
+import * as $$$$$$7 from "./sections/Impact.tsx";
+import * as $$$$$$8 from "./sections/InTheMedia.tsx";
+import * as $$$$$$9 from "./sections/Logos.tsx";
+import * as $$$$$$10 from "./sections/Manifest.tsx";
+import * as $$$$$$11 from "./sections/Partners.tsx";
+import * as $$$$$$12 from "./sections/PartnersTestimonials.tsx";
+import * as $$$$$$13 from "./sections/Pillars.tsx";
+import * as $$$$$$14 from "./sections/Testimonials.tsx";
+import * as $$$$$$15 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$16 from "./sections/WhoWeAre.tsx";
 
 const manifest = {
   "sections": {
@@ -30,15 +31,16 @@ const manifest = {
     "site/sections/Header.tsx": $$$$$$4,
     "site/sections/Hero.tsx": $$$$$$5,
     "site/sections/ImageWithParagraph.tsx": $$$$$$6,
-    "site/sections/InTheMedia.tsx": $$$$$$7,
-    "site/sections/Logos.tsx": $$$$$$8,
-    "site/sections/Manifest.tsx": $$$$$$9,
-    "site/sections/Partners.tsx": $$$$$$10,
-    "site/sections/PartnersTestimonials.tsx": $$$$$$11,
-    "site/sections/Pillars.tsx": $$$$$$12,
-    "site/sections/Testimonials.tsx": $$$$$$13,
-    "site/sections/Theme/Theme.tsx": $$$$$$14,
-    "site/sections/WhoWeAre.tsx": $$$$$$15,
+    "site/sections/Impact.tsx": $$$$$$7,
+    "site/sections/InTheMedia.tsx": $$$$$$8,
+    "site/sections/Logos.tsx": $$$$$$9,
+    "site/sections/Manifest.tsx": $$$$$$10,
+    "site/sections/Partners.tsx": $$$$$$11,
+    "site/sections/PartnersTestimonials.tsx": $$$$$$12,
+    "site/sections/Pillars.tsx": $$$$$$13,
+    "site/sections/Testimonials.tsx": $$$$$$14,
+    "site/sections/Theme/Theme.tsx": $$$$$$15,
+    "site/sections/WhoWeAre.tsx": $$$$$$16,
   },
   "apps": {
     "site/apps/decohub.ts": $$$$$$$$$$$0,
---
+++ sections/Impact.tsx
@@ -0,0 +1,41 @@
+import { RichText } from "apps/admin/widgets.ts";
+
+/**
+ * @title {{{title}}}
+ */
+interface Impact {
+    title?: string;
+    text?: string;
+}
+
+interface Props {
+    title?: string;
+    text?: RichText;
+    impacts?: Impact[]
+}
+
+const backGroundImageGrafism = "https://deco-sites-assets.s3.sa-east-1.amazonaws.com/movtech2030/12359a0f-11e7-43f4-b4de-f77b9c535be6/white-grafism.png"
+const impactPlaceholder:Impact = {title:"999", text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
+export default function Impact({
+    title = "Type your title here",
+    text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+    impacts = [impactPlaceholder, impactPlaceholder, impactPlaceholder, impactPlaceholder]
+}: Props) {
+    return(
+        <div class={`flex flex-col items-center text-center gap-[3.75rem] lg:gap-[6.25rem] py-[3.75rem] px-4  lg:py-[6.25rem] bg-contain bg-top bg-no-repeat`} style={{backgroundImage: `url(${backGroundImageGrafism})`}}>
+            <h1 class={`text-[2rem] lg:text-5xl text-primary font-bold `}>{title}</h1>
+            <div class={`text-2xl lg:text-[2.5rem] lg:px-40`}
+            dangerouslySetInnerHTML={{
+                __html: text
+            }}></div>
+            <div class={`flex flex-col md:flex-row md:flex-wrap md:justify-items-center justify-center items-center gap-10 lg:gap-[5.625rem]`}>
+                    {impacts.map((item) => (
+                        <div class={`flex flex-col gap-6 md:w-[30rem] `}>
+                            <h1 class={`text-5xl lg:text-[3.5rem] text-primary font-bold`}>{item.title}</h1>
+                            <p class={`text-md lg:text-xl text-accent`}>{item.text}</p>
+                        </div>
+                    ))}
+            </div>
+        </div>
+    )
+}
