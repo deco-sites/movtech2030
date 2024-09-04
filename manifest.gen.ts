7e99eb362f211d765ca0bc1d12e58d884b9af51c
vinicius-prates <viniprates2211@gmail.com>
2024-09-03 22:39:42 -0300

    new: Impact Testimonials component

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -12,15 +12,16 @@ import * as $$$$$$4 from "./sections/Header.tsx";
 import * as $$$$$$5 from "./sections/Hero.tsx";
 import * as $$$$$$6 from "./sections/ImageWithParagraph.tsx";
 import * as $$$$$$7 from "./sections/Impact.tsx";
-import * as $$$$$$8 from "./sections/InTheMedia.tsx";
-import * as $$$$$$9 from "./sections/Logos.tsx";
-import * as $$$$$$10 from "./sections/Manifest.tsx";
-import * as $$$$$$11 from "./sections/Partners.tsx";
-import * as $$$$$$12 from "./sections/PartnersTestimonials.tsx";
-import * as $$$$$$13 from "./sections/Pillars.tsx";
-import * as $$$$$$14 from "./sections/Testimonials.tsx";
-import * as $$$$$$15 from "./sections/Theme/Theme.tsx";
-import * as $$$$$$16 from "./sections/WhoWeAre.tsx";
+import * as $$$$$$8 from "./sections/ImpactsTestiomonials.tsx";
+import * as $$$$$$9 from "./sections/InTheMedia.tsx";
+import * as $$$$$$10 from "./sections/Logos.tsx";
+import * as $$$$$$11 from "./sections/Manifest.tsx";
+import * as $$$$$$12 from "./sections/Partners.tsx";
+import * as $$$$$$13 from "./sections/PartnersTestimonials.tsx";
+import * as $$$$$$14 from "./sections/Pillars.tsx";
+import * as $$$$$$15 from "./sections/Testimonials.tsx";
+import * as $$$$$$16 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$17 from "./sections/WhoWeAre.tsx";
 
 const manifest = {
   "sections": {
@@ -32,15 +33,16 @@ const manifest = {
     "site/sections/Hero.tsx": $$$$$$5,
     "site/sections/ImageWithParagraph.tsx": $$$$$$6,
     "site/sections/Impact.tsx": $$$$$$7,
-    "site/sections/InTheMedia.tsx": $$$$$$8,
-    "site/sections/Logos.tsx": $$$$$$9,
-    "site/sections/Manifest.tsx": $$$$$$10,
-    "site/sections/Partners.tsx": $$$$$$11,
-    "site/sections/PartnersTestimonials.tsx": $$$$$$12,
-    "site/sections/Pillars.tsx": $$$$$$13,
-    "site/sections/Testimonials.tsx": $$$$$$14,
-    "site/sections/Theme/Theme.tsx": $$$$$$15,
-    "site/sections/WhoWeAre.tsx": $$$$$$16,
+    "site/sections/ImpactsTestiomonials.tsx": $$$$$$8,
+    "site/sections/InTheMedia.tsx": $$$$$$9,
+    "site/sections/Logos.tsx": $$$$$$10,
+    "site/sections/Manifest.tsx": $$$$$$11,
+    "site/sections/Partners.tsx": $$$$$$12,
+    "site/sections/PartnersTestimonials.tsx": $$$$$$13,
+    "site/sections/Pillars.tsx": $$$$$$14,
+    "site/sections/Testimonials.tsx": $$$$$$15,
+    "site/sections/Theme/Theme.tsx": $$$$$$16,
+    "site/sections/WhoWeAre.tsx": $$$$$$17,
   },
   "apps": {
     "site/apps/decohub.ts": $$$$$$$$$$$0,
---
+++ sections/ImpactsTestiomonials.tsx
@@ -0,0 +1,137 @@
+import { VideoWidget } from "apps/admin/widgets.ts";
+/**
+ * @title {{{personName}}}
+ */
+interface testiomonialWithVideo {
+    text?: string;
+    personName?: string;
+    aboutPerson?: string;
+    video: VideoWidget;
+    placement: "left" | "right";
+}
+
+/**
+ * @title {{{personName}}}
+ */
+interface testimonial {
+    text?: string;
+    personName?: string;
+    aboutPerson?: string;
+}
+
+interface CTA {
+    id?: string;
+    href: string;
+    text: string;
+    outline?: boolean;
+}
+
+export interface Props {
+    title?: string;
+    testiomonialsVideos?: testiomonialWithVideo[];
+    testimonialsText?: testimonial[];
+    subTitle?: string;
+    cta?: CTA;
+
+}
+
+const testimonialVideo_PLACEHOLDER:testiomonialWithVideo = {text: "orem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.", personName: "Vinicius Prates", aboutPerson: "Teacher", placement: "left", video: ""}
+const testimonial_PLACEHOLDER:testimonial = {text: "orem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.", personName: "Vinicius Prates", aboutPerson: "Teacher"}
+const PLACEMENT = {
+    left: "flex-col md:flex-row-reverse",
+    right: "flex-col md:flex-row",
+  };
+
+  
+export default function ImpactsTestiominals({
+    title = "Type your title here",
+    testiomonialsVideos = [testimonialVideo_PLACEHOLDER, testimonialVideo_PLACEHOLDER, testimonialVideo_PLACEHOLDER],
+    testimonialsText = [testimonial_PLACEHOLDER, testimonial_PLACEHOLDER, testimonial_PLACEHOLDER],
+    subTitle = "Type your subTitle here",
+    cta = { id: "change-me-1", href: "/", text: "Change me", outline: false }
+} : Props){
+    return(
+        <div class={`flex flex-col items-center text-center px-4 lg:px-10 gap-[2.5rem] lg:gap-[6.25rem] py-[2.5rem] lg:py-[6.25rem]`}>
+            <h1 class={`text-2xl lg:text-5xl text-accent`}>{title}</h1>
+            <div class={`flex flex-col items-center gap-[2.5rem] lg:gap-[6.25rem]`}>
+                {testiomonialsVideos.map((item) => (
+                     <div class="lg:container md:max-w-6xl lg:mx-auto mx-4 text-sm">
+                     <div
+                       class={`flex ${
+                         PLACEMENT[item.placement]
+                       } gap-12 md:gap-20 text-left items-center z-10`}
+                     >
+                       <iframe  className={`w-[350px] h-[200px] lg:w-[592px] lg:h-[333px] rounded-lg`} src={item.video} frameBorder={0}      
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        class={"rounded-[8px]"} >
+        
+                       </iframe>
+                       <div class="w-full md:w-1/2 space-y-2 md:space-y-4 md:max-w-xl gap-6 z-10  text-left ">
+                            <p class=" text-xs lg:text-xl italic">
+                                {item.text}
+                                </p>
+                        
+                        
+                         <p class="text-sm lg:text-2xl font-bold text-primary">
+                           {item.personName}
+                         </p>
+                        <p class={`text-sm  lg:text-2xl text-primary`}>{item.aboutPerson}</p>
+                         
+                       </div>
+                     </div>
+                   </div>
+                ))}
+            </div>
+            <div class={`grid grid-cols-1 lg:grid-cols-2 items-center justify-center gap-10`}>
+
+            <div className="grid grid-cols-1  gap-6">
+                {testimonialsText.slice(0, 2).map((item, key) => (
+                    <div
+                    key={key}
+                    className={`bg-primary text-info p-6 rounded-[4px] shadow-md flex flex-col`}
+                    >
+                        <p className="text-xl italic text-left mb-4">
+                            {item.text}
+                        </p>
+                        
+                        <div className="flex flex-row gap-2 mt-auto text-left">
+                            <h4 className="text-md font-bold">
+                                {item.personName}
+                            </h4> |
+                            <p className="text-md font-bold">{item.aboutPerson}</p>
+                        </div>
+                    </div>
+                ))}
+                
+            </div>
+            <div
+                    className={`bg-primary text-info  p-6 rounded-[4px] shadow-md flex flex-col`}
+                    >
+                        <p className="text-xl italic text-left mb-4">
+                            "{testimonialsText[2].text}"
+                        </p>
+                        
+                        <div className="flex flex-row gap-2 mt-auto text-left">
+                            <h4 className="text-md font-bold">
+                                {testimonialsText[2].personName}
+                            </h4> | 
+                            <p className="text-md font-bold">{testimonialsText[2].aboutPerson}</p>
+                        </div>
+                    </div>
+            </div>
+            <h1 class={`text-2xl lg:text-[2.5rem] text-primary leading-9 lg:leading-[60px] font-bold`}>{subTitle}</h1>
+
+            <a
+                  href={cta?.href}
+                  target={cta?.href.includes("http") ? "_blank" : "_self"}
+                  class={`font-normal btn btn-primary text-xl text-[#EBFFFD] ${
+                    cta.outline && "btn-outline"
+                  }`}
+                >
+                  {cta?.text}
+                </a>
+        </div>
+    )
+}
+
+
