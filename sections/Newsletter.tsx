c8d257b399bd1cdd64849596318a3b93dc4b0cdd
vinicius-prates <viniprates2211@gmail.com>
2024-09-10 22:29:34 -0300

    fix: change newsletter componente

--- sections/Newsletter.tsx
+++ sections/Newsletter.tsx
@@ -7,11 +7,7 @@ export interface CTA {
 
 export interface Props {
     title?: string;
-    emailInput?: {
-        placeholder: string;
-        cta?: CTA;
-    };
-    cta?: CTA;
+    cta?: CTA[];
     email?: string;
 }
 
@@ -19,16 +15,8 @@ const backgroundGrafism =
     `https://deco-sites-assets.s3.sa-east-1.amazonaws.com/movtech2030/47a62ced-9e3f-4b5d-9f69-4c5deb942413/Background-GRAFISM.png`;
 export default function Newsletter({
     title = "Type you title here",
-    emailInput = {
-        placeholder: "placeholder",
-        cta: {
-            id: "change-me-1",
-            href: "/",
-            text: "Change me",
-            outline: false,
-        },
-    },
-    cta = { id: "change-me-1", href: "/", text: "Change me", outline: false },
+    
+    cta = [{ id: "change-me-1", href: "/", text: "Change me", outline: false }],
     email = "example@gmail.com",
 }: Props) {
     return (
@@ -40,42 +28,32 @@ export default function Newsletter({
                 <h1 class={`text-2xl lg:text-5xl text-info font-bold`}>
                     {title}
                 </h1>
-                <div class={`flex flex-row w-4/5 justify-between mx-10  border border-info  p-2 rounded-lg`}>
-                    <input
-                        class={`bg-transparent placeholder:text-info w-3/5 text-info outline-none`}
-                        placeholder={emailInput?.placeholder}
-                    >
-                    </input>
-                    <a
-                        href={emailInput.cta?.href}
-                        target={emailInput.cta?.href.includes("http")
-                            ? "_blank"
-                            : "_self"}
-                        class={`font-normal  btn btn-info text-sm  lg:text-md text-primary ${
-                            emailInput.cta?.outline && "btn-outline"
-                        }`}
-                    >
-                        {emailInput.cta?.text}
-                    </a>
-                </div>
+                
             </div>
-            <div class={`flex flex-col items-center gap-[0.625rem] lg:gap-5 `}>
+            <div class={`flex flex-col gap-10`}>
+
+            {cta.map((item) => (
+                <div class={`flex flex-col items-center gap-[0.625rem] lg:gap-5 `}>
                 <a
-                    href={cta?.href}
-                    target={cta?.href.includes("http") ? "_blank" : "_self"}
+                    href={item?.href}
+                    target={item?.href.includes("http") ? "_blank" : "_self"}
                     class={`font-normal btn btn-info text-sm  lg:text-xl text-primary ${
-                        cta.outline && "btn-outline"
+                        item.outline && "btn-outline"
                     }`}
-                >
-                    {cta?.text}
+                    >
+                    {item?.text}
                 </a>
-                <a
+                
+            </div>
+            ))}
+            </div>
+            <a
                     href={`MAILTO:${email}`}
                     class={`text-info text-xs lg:text-[1.25rem]`}
-                >
+                    >
                     {email}
                 </a>
-            </div>
+
         </div>
     );
 }
